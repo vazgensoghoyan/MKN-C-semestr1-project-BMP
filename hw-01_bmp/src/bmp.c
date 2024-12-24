a59a1147797b7bc43e59b9f05f7e43dbb66a7f9a
sogvazulchik <sogvazulchik@gmail.com>
2024-12-24 18:50:29 +0300

    return 1 when needed

--- hw-01_bmp/src/bmp.c
+++ hw-01_bmp/src/bmp.c
@@ -7,26 +7,36 @@ int calculate_padding(int size_of_el, int count) {
     return (ALIGNMENT - bytes_in_row % ALIGNMENT) % ALIGNMENT;
 }
 
-bmp_t load_bmp(char *filepath) {
+int load_bmp(char *filepath, bmp_t *bitmap) {
     FILE *file = fopen(filepath, "rb");
 
-    bmp_t bitmap;
+    if (!file) return 1;
 
-    bitmap.fileheader = malloc(sizeof(bmp_fileheader_t));
-    bitmap.v5header = malloc(sizeof(bmp_v5header_t));
+    bitmap->fileheader = malloc(sizeof(bmp_fileheader_t));
+    if (bitmap->fileheader == NULL)
+        return 1;
 
-    fread(bitmap.fileheader, sizeof(bmp_fileheader_t), 1, file);
-    fread(bitmap.v5header, sizeof(bmp_v5header_t), 1, file);
+    bitmap->v5header = malloc(sizeof(bmp_v5header_t));
+    if (bitmap->v5header == NULL)
+        return 1;
 
-    fseek(file, bitmap.fileheader->file_offset_to_pixels, SEEK_SET);
+    fread(bitmap->fileheader, sizeof(bmp_fileheader_t), 1, file);
+    fread(bitmap->v5header, sizeof(bmp_v5header_t), 1, file);
 
-    int height = bitmap.v5header->image_height;
-    int width = bitmap.v5header->image_width;
+    fseek(file, bitmap->fileheader->file_offset_to_pixels, SEEK_SET);
+
+    int height = bitmap->v5header->image_height;
+    int width = bitmap->v5header->image_width;
 
     int padding = calculate_padding(sizeof(pixel_t), width);
     
     pixel_t *pixels = malloc(width * height * sizeof(pixel_t));
+    if (pixels == NULL)
+        return 1;
+
     pixel_t **pxl_array = malloc(height * sizeof(pixel_t *));
+    if (pxl_array == NULL)
+        return 1;
 
     for (int x = 0; x < height; x++) {
         pxl_array[x] = pixels + x * width;
@@ -38,34 +48,49 @@ bmp_t load_bmp(char *filepath) {
         fseek(file, padding, SEEK_CUR);
     }
 
-    bitmap.pixel_array = pxl_array;
+    bitmap->pixel_array = pxl_array;
 
     fclose(file);
 
-    return bitmap;
+    return 0;
 }
 
-bmp_t crop(bmp_t bmp, int x, int y, int width, int height) {
-    bmp_t cropped;
+int crop(bmp_t bmp, bmp_t *cropped, int x, int y, int width, int height) {
+    if (width <= 0 || x < 0 || bmp.v5header->image_width < x + width)
+		return 1;
+    if (height <= 0 || y < 0 || bmp.v5header->image_height < y + height)
+		return 1;
 
-    cropped.fileheader = malloc(sizeof(bmp_fileheader_t));
-    cropped.v5header = malloc(sizeof(bmp_v5header_t));
+    y = bmp.v5header->image_height - y - height;
 
-    *cropped.fileheader = *bmp.fileheader;
-    *cropped.v5header = *bmp.v5header;
+    cropped->fileheader = malloc(sizeof(bmp_fileheader_t));
+    if (cropped->fileheader == NULL)
+        return 1;
+
+    cropped->v5header = malloc(sizeof(bmp_v5header_t));
+    if (cropped->v5header == NULL)
+        return 1;
+
+    *(cropped->fileheader) = *bmp.fileheader;
+    *(cropped->v5header) = *bmp.v5header;
 
     int padding = calculate_padding(sizeof(pixel_t), width);
 
-    cropped.v5header->image_height = height;
-    cropped.v5header->image_width = width;
-    cropped.v5header->image_size = (sizeof(pixel_t) * width + padding) * height;
+    cropped->v5header->image_height = height;
+    cropped->v5header->image_width = width;
+    cropped->v5header->image_size = (sizeof(pixel_t) * width + padding) * height;
 
-    cropped.fileheader->file_size = bmp.fileheader->file_size - \
+    cropped->fileheader->file_size = bmp.fileheader->file_size - \
                                     bmp.v5header->image_size + \
-                                    cropped.v5header->image_size;
+                                    cropped->v5header->image_size;
 
     pixel_t *pixels = malloc(width *height * sizeof(pixel_t));
+    if (pixels == NULL)
+        return 1;
+
     pixel_t **pxl_array = malloc(height * sizeof(pixel_t *));
+    if (pxl_array == NULL)
+        return 1;
 
     for (int i = 0; i < height; i++) {
         pxl_array[i] = pixels + i * width;
@@ -74,37 +99,42 @@ bmp_t crop(bmp_t bmp, int x, int y, int width, int height) {
             pxl_array[i][j] = bmp.pixel_array[x+i][y+j];
     }
 
-    cropped.pixel_array = pxl_array;
+    cropped->pixel_array = pxl_array;
 
-    return cropped;
+    return 0;
 }
 
-bmp_t rotate(bmp_t bmp) {
-    int height = bmp.v5header->image_height;
-    int width = bmp.v5header->image_width;
+int rotate(bmp_t *bmp) {
+    int height = bmp->v5header->image_height;
+    int width = bmp->v5header->image_width;
 
     pixel_t *pixels = malloc(height * width * sizeof(pixel_t));
+    if (pixels == NULL)
+        return 1;
+
     pixel_t **new_pixel_array = malloc(width * sizeof(pixel_t *));
+    if (new_pixel_array == NULL)
+        return 1;
 
     for (int i = 0; i < width; i++) {
         new_pixel_array[i] = pixels + i * height;
 
         for (int j = 0; j < height; j++)
-            new_pixel_array[i][j] = bmp.pixel_array[j][width-i-1];
+            new_pixel_array[i][j] = bmp->pixel_array[j][width-i-1];
     }
 
     int padding = calculate_padding(sizeof(pixel_t), height);
     int new_image_size = (sizeof(pixel_t) * height + padding) * width;
 
-    bmp.fileheader->file_size += new_image_size - bmp.v5header->image_size;
-    bmp.v5header->image_size = new_image_size;
+    bmp->fileheader->file_size += new_image_size - bmp->v5header->image_size;
+    bmp->v5header->image_size = new_image_size;
 
-    bmp.v5header->image_height = width;
-    bmp.v5header->image_width = height;
+    bmp->v5header->image_height = width;
+    bmp->v5header->image_width = height;
 
-    bmp.pixel_array = new_pixel_array;
+    bmp->pixel_array = new_pixel_array;
 
-    return bmp;
+    return 0;
 }
 
 void save_bmp(char *filepath, bmp_t bmp) {
--- hw-01_bmp/src/main.c
+++ hw-01_bmp/src/main.c
@@ -2,8 +2,9 @@
 
 int main(int argc, char **argv) {
     if (argc != 8 || strcmp(argv[1], "crop-rotate") != 0) {
-        return 0;
+        return 1;
     }
+
     char *IN_FILEPATH = argv[2];
     char *OUT_FILEPATH = argv[3];
 
@@ -12,10 +13,19 @@ int main(int argc, char **argv) {
     int W = atoi(argv[6]);
     int H = atoi(argv[7]);
 
-    bmp_t bitmap = load_bmp(IN_FILEPATH);
-    bmp_t cropped = crop(bitmap, X, Y, W, H);
-    bmp_t rotated = rotate(cropped);
-    save_bmp(OUT_FILEPATH, rotated);
+    bmp_t bitmap;
+    bmp_t cropped;
+
+    if (load_bmp(IN_FILEPATH, &bitmap))
+        return 1;
+    
+    if (crop(bitmap, &cropped, X, Y, W, H))
+        return 1;
+
+    if (rotate(&cropped))
+        return 1;
+
+    save_bmp(OUT_FILEPATH, cropped);
 
     free_bmp(bitmap);
     free_bmp(cropped);
