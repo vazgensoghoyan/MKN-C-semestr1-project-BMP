638427b18dd469b03a97309bae10b9955b1fe0fa
sogvazulchik <sogvazulchik@gmail.com>
2024-12-26 22:59:08 +0300

    returned returning 1 when malloc didnt work

--- hw-01_bmp/src/bmp.c
+++ hw-01_bmp/src/bmp.c
@@ -12,7 +12,10 @@ int load_bmp(char *filepath, bmp_t *bitmap) {
     if (!file) return 1;
 
     bitmap->fileheader = malloc(sizeof(bmp_fileheader_t));
+    if (bitmap->fileheader == NULL) return 1;
+
     bitmap->v5header = malloc(sizeof(bmp_v5header_t));
+    if (bitmap->v5header == NULL) return 1;
 
     fread(bitmap->fileheader, sizeof(bmp_fileheader_t), 1, file);
     fread(bitmap->v5header, sizeof(bmp_v5header_t), 1, file);
@@ -25,7 +28,10 @@ int load_bmp(char *filepath, bmp_t *bitmap) {
     int padding = calculate_padding(sizeof(pixel_t), width);
     
     pixel_t *pixels = malloc(width * height * sizeof(pixel_t));
+    if (pixels == NULL) return 1;
+
     pixel_t **pxl_array = malloc(height * sizeof(pixel_t *));
+    if (pxl_array == NULL) return 1;
 
     for (int x = 0; x < height; x++) {
         pxl_array[x] = pixels + x * width;
@@ -49,7 +55,10 @@ int crop(bmp_t bmp, bmp_t *cropped, int x, int y, int width, int height) {
     y = bmp.v5header->image_height - y - height;
 
     cropped->fileheader = malloc(sizeof(bmp_fileheader_t));
+    if (cropped->fileheader == NULL) return 1;
+
     cropped->v5header = malloc(sizeof(bmp_v5header_t));
+    if (cropped->v5header == NULL) return 1;
 
     *(cropped->fileheader) = *bmp.fileheader;
     *(cropped->v5header) = *bmp.v5header;
@@ -62,7 +71,10 @@ int crop(bmp_t bmp, bmp_t *cropped, int x, int y, int width, int height) {
     cropped->fileheader->file_size = cropped->v5header->image_size + cropped->fileheader->file_offset_to_pixels;
 
     pixel_t *pixels = malloc(width *height * sizeof(pixel_t));
+    if (pixels == NULL) return 1;
+
     pixel_t **pxl_array = malloc(height * sizeof(pixel_t *));
+    if (pxl_array == NULL) return 1;
 
     for (int i = 0; i < height; i++) {
         pxl_array[i] = pixels + i * width;
@@ -81,7 +93,10 @@ int rotate(bmp_t *bmp) {
     int width = bmp->v5header->image_width;
 
     pixel_t *pixels = malloc(height * width * sizeof(pixel_t));
+    if (pixels == NULL) return 1;   
+
     pixel_t **new_pixel_array = malloc(width * sizeof(pixel_t *));
+    if (new_pixel_array == NULL) return 1;
 
     for (int i = 0; i < width; i++) {
         new_pixel_array[i] = pixels + i * height;
@@ -106,7 +121,6 @@ int rotate(bmp_t *bmp) {
 
 void save_bmp(char *filepath, bmp_t bmp) {
     FILE *file = fopen(filepath, "wb");
-
     const int ZERO = 0;
 
     fwrite(bmp.fileheader, sizeof(bmp_fileheader_t), 1, file);
